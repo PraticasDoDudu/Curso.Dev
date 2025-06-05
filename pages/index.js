44a6e20c174b7afed124253081975cf0c40ddac9
EduardoLopesBillGates <112276887+PraticasDoDudu@users.noreply.github.com>
2025-06-05 20:30:55 +0000

    Fotos Mama

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ function Home () {
     <div>
       <h1>Mom. I Just Wanna Say: I Love You! :)</h1>
       <p> Versículo Bíblico: "Ela veste de força e dignidade, e rir-se-á do dia que está por vir". "Abre a sua mão ao aflito, e ao necessitado estende as suas mãos." </p>
+      image src= https://www.pexels.com/pt-br/foto/mulher-carregando-bebe-na-praia-durante-o-por-do-sol-51953/
     </div>
   );
 }
