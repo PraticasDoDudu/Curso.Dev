2580ede46090207ebc9b32b9968a9eeac3d6c780
EduardoLopesBillGates <112276887+PraticasDoDudu@users.noreply.github.com>
2025-06-05 20:36:53 +0000

    Mom With Your Baby!

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
+import Image from 'next/image';
+// ...existing code...
 function Home () {
   return (
     <div>
       <h1>Mom. I Just Wanna Say: I Love You! :)</h1>
       <p> Versículo Bíblico: "Ela veste de força e dignidade, e rir-se-á do dia que está por vir". "Abre a sua mão ao aflito, e ao necessitado estende as suas mãos." </p>
-      <img 
+      <Image 
         src="https://images.pexels.com/photos/51953/pexels-photo-51953.jpeg"
         alt="Mulher carregando bebê na praia durante o pôr do sol"
+        width={600}
+        height={400}
         style={{ maxWidth: '100%', height: 'auto' }}
       />
     </div>
   );
 }
-export default Home;
