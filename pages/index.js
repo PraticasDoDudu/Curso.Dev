21e8f0c5e02c2629f7fc30c9f4166a158344e05b
EduardoLopesBillGates <112276887+PraticasDoDudu@users.noreply.github.com>
2025-06-05 20:24:24 +0000

    Mom

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ function Home () {
   return (
     <div>
       <h1>Mom. I Just Wanna Say: I Love You! :)</h1>
+      <p> Versículo Bíblico: "Ela veste de força e dignidade, e rir-se-á do dia que está por vir". "Abre a sua mão ao aflito, e ao necessitado estende as suas mãos." </p>
     </div>
   );
 }
