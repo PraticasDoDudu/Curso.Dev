c844fe7a8500b836188a190fd431b722fd7034a6
EduardoLopesBillGates <112276887+PraticasDoDudu@users.noreply.github.com>
2025-06-05 20:34:16 +0000

    Picture of Mom With Your Baby

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,11 @@ function Home () {
     <div>
       <h1>Mom. I Just Wanna Say: I Love You! :)</h1>
       <p> Versículo Bíblico: "Ela veste de força e dignidade, e rir-se-á do dia que está por vir". "Abre a sua mão ao aflito, e ao necessitado estende as suas mãos." </p>
-      image src= https://www.pexels.com/pt-br/foto/mulher-carregando-bebe-na-praia-durante-o-por-do-sol-51953/
+      <img 
+        src="https://images.pexels.com/photos/51953/pexels-photo-51953.jpeg"
+        alt="Mulher carregando bebê na praia durante o pôr do sol"
+        style={{ maxWidth: '100%', height: 'auto' }}
+      />
     </div>
   );
 }
