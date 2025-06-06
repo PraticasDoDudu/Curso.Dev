db4318fd0d5b496df647966953145eda137fb208
EduardoLopesBillGates <112276887+PraticasDoDudu@users.noreply.github.com>
2025-06-06 13:13:20 +0000

    Recovery

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 function Home () {
   return (
     <div>
-      <h1>Mom. I Just Wanna Say: I Love You! :)</h1>
-      <p>Versículo Bíblico: "Ela Veste De Força E Dignidade, E Rir-Se-A Do Dia Que Está Por Vir".</p>
-      <p>"Abre A Sua Mão Ao Aflito, E Ao Necessitado Estende As Suas Mãos."</p>
+      <p></p>
+      <p>"
+      </p>
     </div>
   );
 }
-export default Home;
+export default Home;                           
