560cab16ded06ad72a9f6637c70a98650fcbb7d1
EduardoLopesBillGates <112276887+PraticasDoDudu@users.noreply.github.com>
2025-06-14 21:59:57 +0000

    pages

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,8 @@ function Home() {
   return (
     <>
       <h1>Official Study Page of Eduardo Antônio Lopes da Silveira ©</h1>
-    <p> Hello, My Friend! </p>
-    image
-    <img src="https://i.imgur.com/https://pt.wikipedia.org/wiki/Get%C3%BAlio_Vargas#/media/Ficheiro:Getulio_Vargas_(1930).jpg" alt="Eduardo Lopes" />
-    <p> This is my official study page. </p>
+      <p> Hello, My Friend! </p>
+      style={{ fontSize: '20px', color: 'blue' }}
     </>
   );
 }
