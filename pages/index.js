4d5b9740df47aafc6b655f9082ce8abad8a1f626
EduardoLopesBillGates <112276887+PraticasDoDudu@users.noreply.github.com>
2025-06-14 21:30:59 +0000

    arquivo `pages`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-  return <h1>Official Study Page of Eduardo Antônio Lopes da Silveira ©</h1>;
+  return (
+    <>
+      <h1>Official Study Page of Eduardo Antônio Lopes da Silveira ©</h1>
+      <h1>Welcome to the Home Page</h1>
+    </>
+  );
 }
 
 export default Home;
+
---
+++ pages/tests/calculadora.test.js
@@ -0,0 +1,10 @@
+test ("Calculadora", callbackFunction); 
+
+function callbackFunction() {
+  console.log("Test Calculadora");
+} 
+
+test ("Test Another Condition Of My System", () => {
+  console.log("Test Another Condition");
+}
+)
