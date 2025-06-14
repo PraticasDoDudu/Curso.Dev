120d5840357c0c22e5f52c3ee7789e15470c14ad
EduardoLopesBillGates <112276887+PraticasDoDudu@users.noreply.github.com>
2025-06-14 21:53:14 +0000

    Pages

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <>
       <h1>Official Study Page of Eduardo Antônio Lopes da Silveira ©</h1>
-      <h1>Hello</h1>
+    <p> Hello, My Friend! </p>
     </>
   );
 }
