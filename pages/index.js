3b7eb7f844cf410fcbfbe2da273dcdd1ee8a2200
EduardoLopesBillGates <112276887+PraticasDoDudu@users.noreply.github.com>
2025-06-15 02:52:38 +0000

    arquivo `pages`

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ function Home() {
   return (
     <>
       <h1>Official Study Page of Eduardo Antônio Lopes da Silveira ©</h1>
+      <div>
+        <h2> I Love Computers ! </h2>
+      </div>
       
       
     </>
