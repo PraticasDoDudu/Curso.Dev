d719045a7cb1a360724345af67738cdb0b50cf2c
EduardoLopesBillGates <112276887+PraticasDoDudu@users.noreply.github.com>
2025-06-15 14:46:26 +0000

    God in Pages

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ function Home() {
       <h1>Official Study Page of Eduardo Antônio Lopes da Silveira ©</h1>
       <div>
         <h2> I Love God And He Will Bless Me </h2>
-        <h3> “Set Your Heart To Discipline And Your Ears To Words Of Knowledge.” To Have Discipline Is To Dominate The Elephant. It Is To Guide Yourself In A Noble Purpose To Be Pronounced Loud And Clear Every Morning, When You Wake Up.</h3>
+        <h3>Set Your Heart To Discipline And Your Ears To Words Of Knowledge. To Have Discipline Is To Dominate The Elephant. It Is To Guide Yourself In A Noble Purpose To Be Pronounced Loud And Clear Every Morning, When You Wake Up.</h3>
       </div>
     
     </>
