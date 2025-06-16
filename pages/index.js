224583496ffbe26edc0440d149917a60885a93a5
EduardoLopesBillGates <112276887+PraticasDoDudu@users.noreply.github.com>
2025-06-16 20:50:01 +0000

    `Pages`

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,11 @@ function Home() {
         <h2> I Love God And He Will Bless Me </h2>
         <h3>Set Your Heart To Discipline And Your Ears To Words Of Knowledge. To Have Discipline Is To Dominate The Elephant. It Is To Guide Yourself In A Noble Purpose To Be Pronounced Loud And Clear Every Morning, When You Wake Up.</h3>
       </div>
-    
+      <div> 
+        <h2> 
+I Love My Family And They Will Love Me Back
+        </h2>
+      </div>
     </>
   );
 }
