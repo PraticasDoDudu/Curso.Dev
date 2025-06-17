548ad35d874b3ff8c3c16bb38509eccf74a14a21
EduardoLopesBillGates <112276887+PraticasDoDudu@users.noreply.github.com>
2025-06-17 13:58:12 +0000

    `scripts`

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,19 @@ function Home() {
 
   return (
     <>
-      <h1>Official Study Page of Eduardo Antônio Lopes da Silveira ©</h1>
-      <div>
-        <h2> I Love God And He Will Bless Me </h2>
-        <h3>Set Your Heart To Discipline And Your Ears To Words Of Knowledge. To Have Discipline Is To Dominate The Elephant. It Is To Guide Yourself In A Noble Purpose To Be Pronounced Loud And Clear Every Morning, When You Wake Up.</h3>
-      </div>
-      <div> 
-        <h2> 
-          I Love My Family And They Will Love Me Back 
-        </h2>
-      </div>
-      <div>
-        God Is The Most Important Thing In My Life. I Will Always Put Him First. I Will Love My Family And They Will Love Me Back. I Will Be A Good Person And Help Others. I Will Work Hard And Be Successful. I Will Be Happy And Enjoy Life.
-      </div>
+    <h1>Official Study Page of Eduardo Antônio Lopes da Silveira ©</h1>
+    <div>
+    <h2> I Love God And He Will Bless Me </h2>
+    <h3>Set Your Heart To Discipline And Your Ears To Words Of Knowledge. To Have Discipline Is To Dominate The Elephant. It Is To Guide Yourself In A Noble Purpose To Be Pronounced Loud And Clear Every Morning, When You Wake Up.</h3>
+    </div>  
+    <div> 
+    <h2> 
+    I Love My Family And They Will Love Me Back 
+    </h2>
+    </div>
+    <div>
+    God Is The Most Important Thing In My Life. I Will Always Put Him First. I Will Love My Family And They Will Love Me Back. I Will Be A Good Person And Help Others. I Will Work Hard And Be Successful. I Will Be Happy And Enjoy Life.
+    </div>
     </>
   );
 }
